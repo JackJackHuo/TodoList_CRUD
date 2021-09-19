89495fe3a1b98fd01713629b6b5dc74014083259
JackHuo <cji4dk40@gmail.com>
2021-09-19 22:54:46 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -38,17 +38,20 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({extended:true}))
 
 // setting routes
+// view all data stored via Todo model
 app.get( '/' , (req , res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
-      .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-      .then(todos => res.render('index', { todos: todos })) // 將資料傳給 index 樣板
-      .catch(error => console.log(error)) // 錯誤處理
+  return Todo.find() // 取出 Todo model 裡的所有資料
+             .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+             .then(todos => res.render('index', { todos: todos })) // 將資料傳給 index 樣板
+             .catch(error => console.log(error)) // 錯誤處理
 })
 
+// render page for creating  new data
 app.get('/todos/new' , (req ,res) => {
   res.render('new')
 })
 
+// view data specified by id
 app.get('/todos/:id' , (req , res) => {
   const id = req.params.id
   return Todo.findById(id)
@@ -57,6 +60,7 @@ app.get('/todos/:id' , (req , res) => {
              .catch(error => console.log(error))
 })
 
+// edit data specified by id
 app.get('/todos/:id/edit' , (req , res) => {
   const id = req.params.id
   return Todo.findById(id)
@@ -65,18 +69,7 @@ app.get('/todos/:id/edit' , (req , res) => {
              .catch(error => console.log(error))
 })
 
-app.post('/todos/:id/edit' , (req , res) => {
-  const id = req.params.id
-  const name = req.body.name
-  return Todo.findById(id) // 用id查詢資料
-             .then(todo => {
-                todo.name = name
-                return todo.save() // 查詢成功後，修改後重新儲存資料
-              })
-             .then(() => res.redirect(`/todos/${id}`))
-             .catch(error => console.log(error))
-}) 
-
+// create
 app.post('/todos' , (req ,res) => {
   const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
   // 方法一
@@ -88,6 +81,28 @@ app.post('/todos' , (req ,res) => {
   return Todo.create({ name: name}) // 存入資料庫
              .then(() => { res.redirect('/') })  // 新增完成後導回首頁
              .catch(error => console.log(error)) // 錯誤處理
+})
+
+// edit
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id) // 用id查詢資料
+    .then(todo => {
+      todo.name = name
+      return todo.save() // 查詢成功後，修改後重新儲存資料
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+// delete
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // setting listen port for server
