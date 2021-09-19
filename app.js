b2a00ce7615c919ad92dac0a14539882e9b9818b
JackHuo <cji4dk40@gmail.com>
2021-09-19 22:20:33 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -54,8 +54,28 @@ app.get('/todos/:id' , (req , res) => {
   return Todo.findById(id)
              .lean()
              .then(todo => { res.render('detail' , {todo : todo})})
-              .catch(error => console.log(error))
+             .catch(error => console.log(error))
 })
+
+app.get('/todos/:id/edit' , (req , res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+             .lean()
+             .then(todo =>  res.render('edit' , {todo : todo}))
+             .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit' , (req , res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id) // 用id查詢資料
+             .then(todo => {
+                todo.name = name
+                return todo.save() // 查詢成功後，修改後重新儲存資料
+              })
+             .then(() => res.redirect(`/todos/${id}`))
+             .catch(error => console.log(error))
+}) 
 
 app.post('/todos' , (req ,res) => {
   const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
@@ -63,11 +83,11 @@ app.post('/todos' , (req ,res) => {
   // const todo = new Todo({name}) // 從Todo產生一個實例
   // return todo.save() // 將實例存入資料庫
   //            .then(() => { res.redirect('/') })  // 新增完成後導回首頁
-  //             .catch(error => console.log(error)) // 錯誤處理
+  //            .catch(error => console.log(error)) // 錯誤處理
   // 方法二
   return Todo.create({ name: name}) // 存入資料庫
-      .then(() => { res.redirect('/') })  // 新增完成後導回首頁
-      .catch(error => console.log(error)) // 錯誤處理
+             .then(() => { res.redirect('/') })  // 新增完成後導回首頁
+             .catch(error => console.log(error)) // 錯誤處理
 })
 
 // setting listen port for server
