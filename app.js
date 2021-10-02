fc8771ad30a58d6f58585e9f92b177a8074751b8
JackHuo <cji4dk40@gmail.com>
2021-10-02 21:31:13 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
 
+
 // import express routes
 //可以省略/index，因為require('資料夾')時，node會自動抓資料夾內名叫index的js檔(預設為index)
 const routes = require('./routes/index') 
@@ -14,8 +15,9 @@ const routes = require('./routes/index')
 require('./config/mongoose')
 // create express server
 const app = express()
-// setting port to which server listens
-const port = 3000
+// 如果在 Heroku 環境則使用 process.env.PORT
+// 否則為本地環境，使用 3000 
+const PORT = process.env.PORT || 3000
 
 
 // setting template engine
@@ -34,7 +36,8 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 // setting listen port for server
-app.listen( 3000 , () => {
-  console.log(`youre now listening http://localhost:${3000}`)
+// 設定應用程式監聽的埠號
+app.listen(PORT , () => {
+  console.log(`youre now listening http://localhost:${PORT}`)
 })
 
