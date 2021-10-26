5780837f5d35155acf7bae8a48d491a291fe925e
JackHuo <cji4dk40@gmail.com>
2021-10-26 17:40:17 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+// users get
 router.get('/login' , (req , res) => {
   res.render('login')
 })
 router.get('/register' , (req , res) => {
   res.render('register')
+})
+// users post
+router.post('/login' , (req , res) => {
+
+})
+router.post('/register' , (req , res) => {
+
 })
 
 module.exports = router
